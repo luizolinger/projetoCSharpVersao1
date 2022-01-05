0d3a65f4cb89290897f9f096cdf6c9d3cc7d36dc
Luiz Otávio Olinger Momm <luizotaviom@gmail.com>
2022-01-05 20:38:49 -0300

    Projeto para testar REPO no GITHUB

---
+++ Cliente.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ProjetoCTest
+{
+    class Cliente
+    {
+        public string Nome;
+        public string Endereco;
+        public bool PessoaFisica;
+
+        public Cliente(string nome, string endereco, bool pessoaFisica)
+        {
+            Nome = nome;
+            Endereco = endereco;
+            PessoaFisica = pessoaFisica;
+        }
+
+        public override string ToString()
+        {
+            return "Nome: " + Nome;
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ProjetoCTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List <Cliente> cliente = new List<Cliente>();
+            cliente.Add(new Cliente("Luiz", "Endereço", true));
+            cliente.Add(new Cliente("Otávio", "Endereço", true));
+
+            foreach (Cliente linha in cliente)
+            {
+                Console.WriteLine(linha);
+            }
+
+        }
+    }
+}
